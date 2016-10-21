8e4c64d68c9794cfa20f46ace3b892896b1e38ca
Marc DeSantis <marc.desantis@wwt.com>
2016-10-21 11:20:51 -0500

    Working API

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
 const path = require('path');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var HttpStatus = require('http-status-codes');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -12,138 +11,45 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/api/v1/todos', (req, res, next) => {
-  const results = [];
-  // Grab data from http request
-  const data = {text: req.body.text, complete: false};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Insert Data
-    client.query('INSERT INTO items(text, complete) values($1, $2)', [data.text, data.complete]);
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+  var db = req.app.get('db');
+  db.items.insert(req.body, function(err, item){
+    return res.status(HttpStatus.CREATED).json(item);
+  });
+});
+
+router.put('/api/v1/todos/:id', (req, res, next) => {
+  var db = req.app.get('db');
+  var item = req.body;
+  item.id = Number(req.params.id);
+  db.items.save(item, function(err, item){
+    return res.status(HttpStatus.OK).json(item);
   });
 });
 
 router.get('/api/v1/todos', (req, res, next) => {
-  const results = [];
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+  var db = req.app.get('db');
+  db.items.find(req.query, function(err, items){
+    return res.json(items);
   });
 });
 
 router.get('/api/v1/todos/:todo_id', (req, res, next) => {
   var result = {};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
+  const id = Number(req.params.todo_id);
+  var db = req.app.get('db');
+  db.items.find(id, function(err, item){
+    if (item) {
+      result = item;
     }
-    const id = req.params.todo_id;
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items WHERE id = ($1);', [id]);
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      result = row;
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(result);
-    });
+    return res.json(item);
   });
 });
 
-router.put('/api/v1/todos/:todo_id', (req, res, next) => {
+router.delete('/api/v1/todos/:id', (req, res, next) => {
   const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
-  // Grab data from http request
-  const data = {text: req.body.text, complete: req.body.complete};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Update Data
-    client.query('UPDATE items SET text=($1), complete=($2) WHERE id=($3)',
-    [data.text, data.complete, id]);
-    // SQL Query > Select Data
-    const query = client.query("SELECT * FROM items ORDER BY id ASC");
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', function() {
-      done();
-      return res.json(results);
-    });
-  });
-});
-
-router.delete('/api/v1/todos/:todo_id', (req, res, next) => {
-  const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Delete Data
-    client.query('DELETE FROM items WHERE id=($1)', [id]);
-    // SQL Query > Select Data
-    var query = client.query('SELECT * FROM items ORDER BY id ASC');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+  var db = req.app.get('db');
+  db.items.destroy({id: Number(req.params.id)}, function(err, item){
+      return res.status(HttpStatus.NO_CONTENT).json(item);
   });
 });
 
